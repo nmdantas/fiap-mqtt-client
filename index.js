9a79d442a5bdaac4cacee964e02c3b46554db4f0
Nicholas M. Dantas <n.moraes.dantas@gmail.com>
2020-04-01 16:08:16 -0300

    User api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mqtt = require('./service/mqtt');
+const repository = require('./repository/postgre');
 const express = require('express');
 const bodyParser  = require('body-parser');
 const app = express();
@@ -13,6 +14,46 @@ app.post('/api/v1/messages', (req, res) => {
     const result = mqtt.publish(req.body);
     
     res.json(result);
+});
+
+app.get('/api/v1/users', async (req, res) => {
+    const result = await repository.user.findAll();
+    
+    if (result.length > 0) {
+        res.json(result);
+    } else {
+        res.status(404).end();
+    }
+});
+
+app.post('/api/v1/users', async (req, res) => {
+    const result = await repository.user.create(req.body.name);
+    
+    if (result) {
+        res.json(result);
+    } else {
+        res.status(400).end();
+    }
+});
+
+app.get('/api/v1/users/:id', async (req, res) => {
+    const result = await repository.user.findById(req.params.id);
+    
+    if (result) {
+        res.json(result);
+    } else {
+        res.status(404).end();
+    }
+});
+
+app.delete('/api/v1/users/:id', async (req, res) => {
+    const result = await repository.user.delete(req.params.id);
+    
+    if (result) {
+        res.json(result);
+    } else {
+        res.status(404).end();
+    }
 });
    
 app.listen(process.env.PORT || 3000);
