165afbc24190ffa7a582c810bbd4d246c2dfad8a
Nicholas M. Dantas <n.moraes.dantas@gmail.com>
2020-04-01 01:02:42 -0300

    Express

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const REDIS_URL = process.env.REDIS_URL || 'redis://h:pd3a1b8206963eea358af12e9ba519a1a39a0105c4e2df6d397744b6f3bfe268d@ec2-52-202-177-173.compute-1.amazonaws.com:12469';
 const DATABASE_URL = process.env.DATABASE_URL || 'postgres://taussjwjzozqga:764f36b6b3a9f0dd85ca7e2143ef24302bb0f283096e4ac2cd0c2bc80120296e@ec2-52-207-93-32.compute-1.amazonaws.com:5432/d9p6h72vsmi2k6';
 
+const express = require('express');
 const mqtt = require('mqtt');
-const redis = require('redis');
 const postgre = require('pg');
 
+const app = express();
 const mqttClient = mqtt.connect('mqtt://likyjqkw:Zwx1-XnEvWgV@tailor.cloudmqtt.com:14970');
-const redisClient = redis.createClient(REDIS_URL);
-const pool = new postgre.Pool({
+const databasePool = new postgre.Pool({
     connectionString: DATABASE_URL
 });
 
@@ -17,38 +16,28 @@ const createUser = (name) => {
     const params = [];
     params.push(name);
 
-    pool.query('insert into users (name) values ($1) returning *', params, (error, response) => {
+    databasePool.query('insert into users (name) values ($1) returning *', params, (error, response) => {
         if (error) {
             console.error('POSTGRE:Error');
             console.error(error);
         } else {
-            console.info('POSTGRE:Insert:' + response.rows[0]);
+            console.info('POSTGRE:Insert:' + JSON.stringify(response.rows[0]));
         }
     });
 };
 
-redisClient.on('ready', () => {
-    console.info('REDIS:Ready');
-});
+const startDate = new Date();
+const messages = [];
 
-redisClient.on('connect', () => {
-    console.info('REDIS:Connect');
+app.get('/', function (req, res) {
+    res.json({
+        since: startDate,
+        count: messages.length,
+        itens: messages
+    });
 });
-
-redisClient.on('error', (error) => {
-    console.error('REDIS:Error');
-    console.error(error);
-});
-
-redisClient.get('test/user', (error, reply) => {
-    if (error) {
-        console.error('REDIS:Error');
-        console.error(error);
-    } else {
-        console.info('REDIS:Get');
-        console.info(reply);
-    }
-});
+   
+app.listen(process.env.PORT || 3000);
 
 mqttClient.on('connect', () => {
     console.info('MQTT:Connect');
@@ -59,11 +48,14 @@ mqttClient.on('connect', () => {
 mqttClient.on('message', (topic, message) => {
     console.info('MQTT:Message:' + topic);
 
+    messages.push({
+        topic: topic,
+        content: message.toString()
+    });
+
     if (topic === 'test/user') {
         const userName = message.toString();
         console.info(userName);
-
-        redisClient.set(topic, userName);
 
         createUser(userName);
     }
